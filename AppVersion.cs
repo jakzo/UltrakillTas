9802732f676a6da3f240c2ed52b40f222973c217
jakzo <jack@jf.id.au>
2023-01-16 22:19:27 +1100

    first commit

---
+++ AppVersion.cs
@@ -0,0 +1,3 @@
+namespace Utas {
+static class AppVersion { public const string Value = "0.0.0"; }
+}
---
+++ src/Mod.cs
@@ -0,0 +1,18 @@
+﻿using MelonLoader;
+
+namespace Utas {
+public static class BuildInfo {
+  public const string Name = "UltrakillTas";
+  public const string Author = "jakzo";
+  public const string Company = null;
+  public const string Version = AppVersion.Value;
+  public const string DownloadLink =
+      "https://ultrakill.thunderstore.io/package/jakzo/UltrakillTas/";
+
+  public const string Developer = "Newblood";
+  public const string GameName = "Ultrakill";
+}
+
+public class Mod : MelonMod {
+}
+}
